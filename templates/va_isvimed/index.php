a02f06bf6de08f0627cf255e4fd60dee5f53c24d
Victor Arias <hugovic1182@gmail.com>
2012-09-07 18:20:38 -0500

    Agregado analytics y más imagenes para el encabezado

--- templates/va_isvimed/index.php
+++ templates/va_isvimed/index.php
@@ -189,7 +189,17 @@ $urls = substr(trim($urls), 0, -1);
 			</footer>
 			<div id="copyright"><jdoc:include type="modules" name="copyright" style="none" /></div>
 		</div>
+		<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34649352-1']);
+  _gaq.push(['_trackPageview']);
 
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 		<jdoc:include type="modules" name="debug" />
 	</body>
 </html>
